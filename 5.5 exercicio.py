35d3c006c9bbacfe0cbe1e7fc29d12c7eb89752a
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-05-03 14:24:26 +0000

    reprovado

---
+++ 5.5 exercicio.py
@@ -0,0 +1,24 @@
+import os
+os.system("cls || clear")
+
+print("Solicitando dados")
+nome = input("Digite o nome do aluno: ")
+idade = input("Digite a idade do aluno: ")
+notaUm = int(input("Digite a primeira nota: "))
+notaDois = int(input("Digite a segunda nota: "))
+notaTres = int(input("Digite a terceira nota: "))
+soma = notaUm + notaDois
+media = soma / 2
+
+print("jonExibindo dados")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Primeira nota: {notaUm}")
+print(f"Segunda nota: {notaDois}")
+print(f"Segunda terceira: {notaTres}")
+print(f"Média: {media}")
+
+if media  >= 7:
+    print("Aprovado.")
+else:
+    print("Reprovado")
