ef2688b40e8af39fa05ceebf6f337a07e962c4be
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-05-03 12:23:25 +0000

    python for jonga

---
+++ 3.Tipagem estática.py
@@ -0,0 +1,18 @@
+# Tipagem dinâmica.
+
+#Atribuindo valores.
+nome: str = "Marta"
+idade: int = "20"
+peso: float = 62.500
+
+#Exibindo dados.
+
+#Simples.
+print(nome)
+print(idade)
+print(peso)
+
+#Formatado.
+print(f"Nome : {nome}")
+print(f"Idade : {idade}")
+print(f"Peso: {peso}")
